5f693bcbef0b9b4465653acff2b9b0e1652796b0
Alessandro Cifani <alessandro.cifani@adevinta.com>
2023-04-03 14:20:29 +0200

    feat: show last 6m

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ bot.onText(/https?:\/\/(www\.)?amazon\.[a-zA-Z0-9-\.]{2,}\//, (msg) => {
   const asin = msg.text?.match(/(?<=dp\/)[A-Z0-9]{10}/)?.[0]
 
   if (asin) {
-    const graph = `https://charts.camelcamelcamel.com/it/${asin}/amazon-new-used.png?force=1&zero=0&w=600&h=360&desired=false&legend=1&ilt=1&tp=all&fo=0&lang=it`
+    const graph = `https://charts.camelcamelcamel.com/it/${asin}/amazon-new-used.png?force=1&zero=0&w=600&h=360&desired=false&legend=1&ilt=1&tp=6m&fo=0&lang=it`
     bot.sendPhoto(chatId, graph, {
       caption: `https://it.camelcamelcamel.com/product/${asin}`,
       message_thread_id: msg.message_thread_id,
