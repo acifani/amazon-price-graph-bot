c4cb8f548b370db1e3cf9831378b7cef7ff42337
Alessandro Cifani <alessandro.cifani@gmail.com>
2023-04-03 13:38:56 +0200

    feat: support threads

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,11 @@ bot.onText(/https?:\/\/(www\.)?amazon\.[a-zA-Z0-9-\.]{2,}\//, (msg) => {
   const chatId = msg.chat.id
   const asin = msg.text?.match(/(?<=dp\/)[A-Z0-9]{10}/)?.[0]
 
-  console.log(msg)
-
   if (asin) {
-    const graph = `https://charts.camelcamelcamel.com/it/${asin}/amazon-new-used.png?force=1&zero=0&w=855&h=513&desired=false&legend=1&ilt=1&tp=all&fo=0&lang=it`
-    bot.sendPhoto(chatId, graph, { caption: `https://it.camelcamelcamel.com/product/${asin}` })
+    const graph = `https://charts.camelcamelcamel.com/it/${asin}/amazon-new-used.png?force=1&zero=0&w=600&h=360&desired=false&legend=1&ilt=1&tp=all&fo=0&lang=it`
+    bot.sendPhoto(chatId, graph, {
+      caption: `https://it.camelcamelcamel.com/product/${asin}`,
+      message_thread_id: msg.message_thread_id,
+    })
   }
 })
