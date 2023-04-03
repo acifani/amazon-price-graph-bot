c0d264be9697d759feb6e5afec7f4338f8c20c5d
Alessandro Cifani <alessandro.cifani@gmail.com>
2023-04-03 22:57:25 +0200

    feat: multiple links + shorteners

--- src/index.ts
+++ src/index.ts
@@ -8,31 +8,51 @@ const customChatID = Number(process.env.CUSTOM_CHAT_ID)
 const customChatTopicFrom = Number(process.env.CUSTOM_CHAT_TOPIC_FROM)
 const customChatTopicTo = Number(process.env.CUSTOM_CHAT_TOPIC_TO)
 
-bot.onText(/https?:\/\/(www\.)?amazon\.[a-zA-Z0-9-\.]{2,}\//, (msg) => {
-  const chatId = msg.chat.id
-  const asin = msg.text?.match(/(?<=dp\/)[A-Z0-9]{10}/)?.[0]
+bot.onText(/https?:\/\/(?:www\.)?amazon\.it\//, (msg) => {
+  const matches = msg.text?.match(/(?<=dp\/)[A-Z0-9]{10}/)
+  matches?.forEach((asin) => sendProductMessages(asin, msg))
+})
 
-  if (asin) {
-    // const camelGraph = `https://charts.camelcamelcamel.com/it/${asin}/amazon-new-used.png?force=1&zero=0&w=600&h=360&desired=false&legend=1&ilt=1&tp=6m&fo=0&lang=it`
-    const keepaGraph = `https://graph.keepa.com/pricehistory.png?asin=${asin}&domain=it&salesrank=1&amazon=1&new=1&used=1&bb=0&fba=0&fbm=0&pe=0&ld=1&bbu=0&wd=0&range=180&width=600&height=300`
+bot.onText(/https?:\/\/(amzn\.to|voob\.it)\//, async (msg) => {
+  const links = msg.text?.match(/https?:\/\/(amzn\.to|voob\.it)\/(\w+)/g)
+  if (!links) {
+    console.warn('Could not find links for', msg.text)
+    return
+  }
 
-    const photoOptions: TelegramBot.SendPhotoOptions = {
-      disable_notification: true,
-      message_thread_id: msg.message_thread_id,
-      reply_markup: {
-        inline_keyboard: [
-          [
-            { text: 'Keepa', url: `https://keepa.com/#!product/8-${asin}` },
-            { text: 'CamelCamelCamel', url: `https://it.camelcamelcamel.com/product/${asin}` },
-          ],
-        ],
-      },
-    }
+  for (const link of links) {
+    const res = await fetch(link, { redirect: 'manual' })
+    const amznLink = res.headers.get('Location')
+    const asin = amznLink?.match(/(?<=dp\/)[A-Z0-9]{10}/)?.[0]
 
-    bot.sendPhoto(chatId, keepaGraph, photoOptions)
-
-    if (chatId === customChatID && msg.message_thread_id === customChatTopicFrom) {
-      bot.sendPhoto(chatId, keepaGraph, { ...photoOptions, message_thread_id: customChatTopicTo })
+    if (asin) {
+      sendProductMessages(asin, msg)
     }
   }
 })
+
+function sendProductMessages(asin: string, msg: TelegramBot.Message) {
+  const chatId = msg.chat.id
+
+  // const camelGraph = `https://charts.camelcamelcamel.com/it/${asin}/amazon-new-used.png?force=1&zero=0&w=600&h=360&desired=false&legend=1&ilt=1&tp=6m&fo=0&lang=it`
+  const keepaGraph = `https://graph.keepa.com/pricehistory.png?asin=${asin}&domain=it&salesrank=1&amazon=1&new=1&used=1&bb=0&fba=0&fbm=0&pe=0&ld=1&bbu=0&wd=0&range=180&width=600&height=300`
+
+  const photoOptions: TelegramBot.SendPhotoOptions = {
+    disable_notification: true,
+    message_thread_id: msg.message_thread_id,
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: 'Keepa', url: `https://keepa.com/#!product/8-${asin}` },
+          { text: 'CamelCamelCamel', url: `https://it.camelcamelcamel.com/product/${asin}` },
+        ],
+      ],
+    },
+  }
+
+  bot.sendPhoto(chatId, keepaGraph, photoOptions)
+
+  if (chatId === customChatID && msg.message_thread_id === customChatTopicFrom) {
+    bot.sendPhoto(chatId, keepaGraph, { ...photoOptions, message_thread_id: customChatTopicTo })
+  }
+}
